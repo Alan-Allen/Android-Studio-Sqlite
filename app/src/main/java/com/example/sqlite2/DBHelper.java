f7675130954a9e7675b233635dd70ef51b0cbab6
Alan-Allen <11146001@ntub.edu.tw>
2024-03-15 13:07:00 +0800

    school-2024/3/15/01:06

--- app/src/main/java/com/example/sqlite2/DBHelper.java
+++ app/src/main/java/com/example/sqlite2/DBHelper.java
@@ -1,5 +1,6 @@
 package com.example.sqlite2;
 
+import android.annotation.SuppressLint;
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
@@ -7,6 +8,9 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class DBHelper extends SQLiteOpenHelper {
     String TableName;
@@ -32,6 +36,21 @@ public class DBHelper extends SQLiteOpenHelper {
         db.execSQL(SQL);
     }
 
+    public void chickTable(){
+        Cursor cursor = getWritableDatabase().rawQuery(
+                "select DISTINCT tbl_name from sqlite_master where tbl_name = '" + TableName + "'", null);
+        if (cursor != null) {
+            if (cursor.getCount() == 0)
+                getWritableDatabase().execSQL("CREATE TABLE IF NOT EXISTS " + TableName + "( " +
+                        "_id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                        "name TEXT, " +
+                        "user TEXT," +
+                        "password TEXT" +
+                        ");");
+            cursor.close();
+        }
+    }
+
     public void insert(String name, String user, String password) {
         SQLiteDatabase db = getWritableDatabase();
         ContentValues values = new ContentValues();
@@ -39,5 +58,65 @@ public class DBHelper extends SQLiteOpenHelper {
         values.put("user", user);
         values.put("password", password);
         db.insert(TableName, null, values);
+    }
+
+    public void modify(String id, String name, String user, String password) {
+        SQLiteDatabase db = getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put("name", name);
+        values.put("user", user);
+        values.put("password", password);
+        db.update(TableName, values, "_id = " + id, null);
+    }
+
+    public void delete(String id) {
+        SQLiteDatabase db = getWritableDatabase();
+        db.delete(TableName, "_id = " + id, null);
+    }
+
+    public void deleteAll() {
+        SQLiteDatabase db = getWritableDatabase();
+        db.execSQL("DELETE FROM " + TableName);
+    }
+
+    public ArrayList<HashMap<String, String>> searchAll() {
+        SQLiteDatabase db = getWritableDatabase();
+        @SuppressLint("Recycle") Cursor c = db.rawQuery("SELECT * FROM " + TableName, null);
+        ArrayList<HashMap<String, String>> arrayList = new ArrayList<>();
+        while(c.moveToNext()) {
+            HashMap<String, String> hashMap = new HashMap<>();
+            String id = c.getString(0);
+            String name = c.getString(1);
+            String user = c.getString(2);
+            String password = c.getString(3);
+
+            hashMap.put("id", id);
+            hashMap.put("name", name);
+            hashMap.put("user", user);
+            hashMap.put("password", password);
+            arrayList.add(hashMap);
+        }
+        return arrayList;
+    }
+
+    public ArrayList<HashMap<String, String>> search(String ID) {
+        SQLiteDatabase db = getWritableDatabase();
+        @SuppressLint("Recycle") Cursor c = db.rawQuery("SELECT * FROM " + TableName
+                + " WHERE _id = " + "'" + ID + "'", null);
+        ArrayList<HashMap<String, String>> arrayList = new ArrayList<>();
+        while(c.moveToNext()) {
+            HashMap<String, String> hashMap = new HashMap<>();
+            String id = c.getString(0);
+            String name = c.getString(1);
+            String user = c.getString(2);
+            String password = c.getString(3);
+
+            hashMap.put("id", id);
+            hashMap.put("name", name);
+            hashMap.put("user", user);
+            hashMap.put("password", password);
+            arrayList.add(hashMap);
+        }
+        return arrayList;
     }
 }
--- app/src/main/java/com/example/sqlite2/MainActivity.java
+++ app/src/main/java/com/example/sqlite2/MainActivity.java
@@ -1,28 +1,47 @@
 package com.example.sqlite2;
 
+import static android.content.ContentValues.TAG;
+
 import android.annotation.SuppressLint;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.textfield.TextInputEditText;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
     private final String DBName = "userList.db";
-    private String TableName = "userTalbe";
+    private String TableName = "userTable";
     private final int DBVersion = 1;
     DBHelper dbHelper;
 
+    ArrayList<HashMap<String, String>> arrayList = new ArrayList<>();
+    ArrayList<HashMap<String, String>> getNowArray = new ArrayList<>();
+
+    Button insert, delete, update;
+    EditText name, user, password;
+    MyAdapter myAdapter;
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -31,15 +50,108 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        Button insert = findViewById(R.id.Insert);
-        Button delete = findViewById(R.id.delete);
-        Button update = findViewById(R.id.updata);
-        EditText name = findViewById(R.id.Name);
-        EditText user = findViewById(R.id.User);
-        EditText password = findViewById(R.id.Password);
-
         dbHelper = new DBHelper(this, DBName, null, DBVersion, TableName);
+        dbHelper.chickTable();
+        arrayList = dbHelper.searchAll();
+        itemSetting();//連接所有元件
+        recyclerViewSetting();//設置RecyclerView
+        buttonFunction();//設置按鈕功能
+        dbHelper.insert("Admin", "admin", "1234");
 
         SQLiteDatabase database = dbHelper.getWritableDatabase();
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    private void buttonFunction() {//設置按鈕功能
+        delete.setOnClickListener(v -> {
+            clearAll();//清空目前所選以及所有editText
+        });
+        insert.setOnClickListener(v -> {
+            dbHelper.insert(name.getText().toString()
+                    ,user.getText().toString()
+                    ,password.getText().toString());
+            arrayList = dbHelper.searchAll();
+            myAdapter.notifyDataSetChanged();
+            clearAll();
+        });
+        update.setOnClickListener(v -> {
+            dbHelper.modify(getNowArray.get(0).get("id")
+                    ,name.getText().toString()
+                    ,user.getText().toString()
+                    ,password.getText().toString());
+            arrayList = dbHelper.searchAll();
+            myAdapter.notifyDataSetChanged();
+            clearAll();
+
+        });
+    }
+
+    private void clearAll() {//清空目前所選以及所有editText
+        name.setText("");
+        user.setText("");
+        password.setText("");
+        getNowArray.clear();
+    }
+
+    private void recyclerViewSetting() {//設置RecyclerView
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        myAdapter = new MyAdapter();
+        recyclerView.setAdapter(myAdapter);
+    }
+
+    private void itemSetting() {//連接所有元件
+        insert = findViewById(R.id.Insert);
+        delete = findViewById(R.id.delete);
+        update = findViewById(R.id.updata);
+        name = findViewById(R.id.Name);
+        user = findViewById(R.id.User);
+        password = findViewById(R.id.Password);
+    }
+
+
+    private class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {//設置Adapter
+
+        @NonNull
+        @Override
+        public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext())
+                    .inflate(android.R.layout.simple_list_item_1, null);
+            return new ViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+            holder.tvTitle.setText(arrayList.get(position).get("name"));
+
+            holder.itemView.setOnClickListener((v) -> {
+                getNowArray.clear();
+                getNowArray = dbHelper.search(arrayList.get(position).get("id"));
+                try {
+                    name.setText(getNowArray.get(0).get("name"));
+                    user.setText(getNowArray.get(0).get("user"));
+                    password.setText(getNowArray.get(0).get("password"));
+                } catch (Exception e) {
+                    Log.d(TAG, "onBindViewHolder: " + e.getMessage());
+                }
+
+            });
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return arrayList.size();
+        }
+
+        public class ViewHolder extends RecyclerView.ViewHolder {
+            TextView tvTitle;
+
+            public ViewHolder(@NonNull View itemView) {
+                super(itemView);
+                tvTitle = itemView.findViewById(android.R.id.text1);
+            }
+        }
     }
 }
