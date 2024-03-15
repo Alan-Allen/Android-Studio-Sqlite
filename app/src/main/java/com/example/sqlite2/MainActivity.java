f718680cc1f96c733f9c57991a3f86c5bb832169
Alan-Allen <11146001@ntub.edu.tw>
2024-03-15 10:07:24 +0800

    first commit

---
+++ app/src/main/java/com/example/sqlite2/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.sqlite2;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button insert = findViewById(R.id.Insert);
+    Button delete = findViewById(R.id.delete);
+    Button update = findViewById(R.id.updata);
+    EditText name = findViewById(R.id.Name);
+    EditText user = findViewById(R.id.User);
+    EditText password = findViewById(R.id.Password);
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
